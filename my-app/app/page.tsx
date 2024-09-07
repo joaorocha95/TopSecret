34b36a22a6f302e92552ba3dece470a30fe333ed
Joao <joaorocha95@hotmail.com>
2024-09-07 23:46:50 +0100

    a

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           <li className="mb-2">
             Get started by edit{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsxawffffffffffffffffffffffffffff
+              app/page.tsx
             </code>
             .
           </li>
